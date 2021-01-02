05bf17d3b4f4c45214823ac0f6d15e84e40d4ca6
cedd15 <cedricvincenth@yahoo.com>
2021-01-03 05:31:17 +0800

    Python - While Loop

--- app.py
+++ app.py
@@ -1,38 +1,6 @@
-myFruits = {
-    "fruit1" : {
-        "name" : "apple",
-        "color" : "red"
-    },
-    "fruit2" : {
-        "name" : "banana",
-        "color" : "yellow"
-    },
-    "fruit3" : {
-        "name" : "grapes",
-        "color" : "purple"
-    }
-}
-
-print(myFruits)
-
-game1 = {
-    "title" : "COD",
-    "genre" : "shooting"
-}
-game2 = {
-    "title" : "Uncharted",
-    "gengre" : "adventure"
-}
-game3 = {
-    "title" : "Last of Us",
-    "genre" : "zombie"
-}
-
-myGames = {
-    "game1" : game1,
-    "game2" : game2,
-    "game3" : game3
-
-}
-
-print(myGames)
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+else:
+    print("i is no longer than 5")
