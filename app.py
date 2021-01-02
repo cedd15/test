a6b94d9eb9a4e7f4f8a1aa29961eacc82d9c9814
cedd15 <cedricvincenth@yahoo.com>
2021-01-03 03:40:23 +0800

    Python Dictionary - Nested Dictionaries

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
-dict = {
-    "fruit": "apple",
-    "size": "large",
-    "texture": "fresh"
+myFruits = {
+    "fruit1" : {
+        "name" : "apple",
+        "color" : "red"
+    },
+    "fruit2" : {
+        "name" : "banana",
+        "color" : "yellow"
+    },
+    "fruit3" : {
+        "name" : "grapes",
+        "color" : "purple"
+    }
 }
-if "price" in dict:
-    print("Yes, the price is available.\n")
 
-else:
-    print("No price\n")
+print(myFruits)
+
+game1 = {
+    "title" : "COD",
+    "genre" : "shooting"
+}
+game2 = {
+    "title" : "Uncharted",
+    "gengre" : "adventure"
+}
+game3 = {
+    "title" : "Last of Us",
+    "genre" : "zombie"
+}
+
+myGames = {
+    "game1" : game1,
+    "game2" : game2,
+    "game3" : game3
+
+}
+
+print(myGames)
