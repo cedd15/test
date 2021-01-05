9acd09da0bfc3dd181f1ae955e561fb99fc512c8
Cedd <cedd.hije@outlook.com>
2021-01-06 03:24:28 +0800

    Python - For Loops(Else in For Loop)

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-#The range() function defaults to increment the sequence by 1,
-# however it is possible to specify the increment value
-# by adding a third parameter: (5, 100, 5)
-for y in range(5, 100, 5):
-    print(y)
+for x in range(6):
+        print(x)
+else:
+    print("Done!")
+
+#The else keyword in a for loop
+#specifies a block of code to be executed
+#when the loop is finished:
+
