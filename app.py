2940d0279dff7342a4eac84c7bc4c1970eb0ae59
Cedd <cedd.hije@outlook.com>
2021-01-07 08:00:55 +0800

    Python - Lambda as Anonymous Function

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
-x = lambda x, y : x * y
-print(x(5,5))
+def myfunc(x):
+    return lambda a: a * x
+
+mydoubler = myfunc(2) #variable "mydoubler" is used to name the lambda
+#the value of this variable is the function which should have an argument and that is 2
+
+
+print(mydoubler(5)) #we are now calling lambda with another argument
