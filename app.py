43683624ce11a6528001df8e838fa01f60257b65
Cedd <cedd.hije@outlook.com>
2021-01-08 03:44:59 +0800

    Python - Inheritance

--- app.py
+++ app.py
@@ -5,9 +5,24 @@ class Person:
 
     def intro(self):
         print("Hi, my name is " + self.name)
-        print("I am " + str(self.age)) # can't concatenate int to string hence the use of str()
+        print("I am " + str(self.age))  # can't concatenate int to string hence the use of str()
+
 
 p1 = Person("John", 36)
 
 p1.intro()
 
+
+class Student(Person):
+    def __init__(self, name, age,city):
+        super().__init__(name, age)
+        self.city = city
+
+    def comp(self):
+        print("Hi, my name is " + self.name, "\nI am " + str(self.age), "\nI live in " + self.city)
+
+
+
+
+x = Student("Mike", 20, "Manila")
+x.comp()
