33bb058ebcc62eb38cadcc73455ab7a64fba365c
Cedd <cedd.hije@outlook.com>
2021-01-09 08:36:19 +0800

    Python - Iteration

--- app.py
+++ app.py
@@ -1,31 +1,22 @@
-class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+class Nums:
+    def __iter__(self):
+        self.a = 1
+        return self
 
-    def intro(self):
-        print("Hi, my name is " + self.name)
-        print("I am " + str(self.age))  # can't concatenate int to string hence the use of str()
+    @property
+    def __next__(self):
+        if self.a <=20:
+            x = self.a
+            self.a +=1
+            return x
+        else:
+            raise StopIteration
 
+myclass = Nums()
+myiter = iter(myclass)
 
-p1 = Person("John", 36)
-
-p1.intro()
-
-
-class Student(Person):
-    def __init__(self, name, age,city):
-        super().__init__(name, age)
-        self.city = city
-
-    def comp(self):
-        print("Hi, my name is " + self.name, "\nI am " + str(self.age), "\nI live in " + self.city)
-
-
-
-
-x = Student("Mike", 20, "Manila")
-x.comp()
-
-
-# another test
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
